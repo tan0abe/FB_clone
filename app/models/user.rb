f00ee5af0f5cbf1414c750c4cc6e52490a820f18
ryota tanabe <teni0abe@gmail.com>
2019-05-11 13:40:23 +0900

    [update]ユーザー登録時にすでに使われているemailを使えないようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, uniqueness: true,
+                    presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
                     before_validation { email.downcase! }
   has_secure_password
