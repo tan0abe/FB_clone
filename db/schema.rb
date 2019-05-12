27fa910def2701451d31fd35656521f0a3cb58f8
ryota tanabe <teni0abe@gmail.com>
2019-05-12 15:46:07 +0900

    [?]user_imageカラムを追加しただけのつもりがfavoritesテーブルが出来ていた

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_073955) do
+ActiveRecord::Schema.define(version: 2019_05_08_114331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -31,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_073955) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "user_image"
     t.string "image_path"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
